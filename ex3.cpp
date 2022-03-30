e6affe4c8d634bbba750a14b7b210a73e8255f19
avivzeh1 <100088843+avivzeh1@users.noreply.github.com>
2022-03-30 21:29:30 +0300

    Update ex3.cpp

--- ex3.cpp
+++ ex3.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-int test = 0;
 #include "Controller.h"
 #include "Model.h"
 
@@ -15,37 +14,18 @@ public:
 	}
 } md;
 
-//int main(int argc, char* args[]) {
-//
-//
-//	try{
-//	if(argc != 3)
-//		throw "ERROR: number of input files is illegal";
-//	Controller c;
-//	c.run(args);
-//	
-//	}catch(const char *e){
-//		cerr << e;
-//	}
-//
-//	return 0;
-//}
+int main(int argc, char* args[]) {
 
-template<class T>
-void swa(T& a, T& b) {
-T tmp = {move(a)};
-a = move(b);
-b = move(tmp);
-}
 
-int main()
-{
-	int a = 5;
-	int y = 42;
-	int *arr = new int[10];
-	arr[5] = 8;
-swa(a,y);
-printf("%d",arr[5]);
-printf("%d",y);
-return 0;
+	try{
+	if(argc != 3)
+		throw "ERROR: number of input files is illegal";
+	Controller c;
+	c.run(args);
+	
+	}catch(const char *e){
+		cerr << e;
+	}
+
+	return 0;
 }
